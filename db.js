3cedafbac73f0b14f1de5ae3847a12e71e2ccfa1
Davidzh123 <davidzhoou045@gmail.com>
2024-11-29 15:22:15 +0100

    connexion bdd

--- db.js
+++ db.js
@@ -1,38 +1,24 @@
-// db.js
+const { Sequelize } = require('sequelize')
 
-require('dotenv').config(); // Charge les variables d'environnement en local
-
-const { Sequelize } = require('sequelize');
-
-// Utilisez `DATABASE_URL` si disponible, sinon construisez la chaîne de connexion
-const DATABASE_URL = process.env.DATABASE_URL || `postgres://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.PORT}/${process.env.DB_NAME}`;
-
-// Créer une instance de Sequelize en utilisant la chaîne de connexion
-const sequelize = new Sequelize(DATABASE_URL, {
+// Database
+const sequelize = new Sequelize(
+    process.env.DATABASE_URL, // TODO: database connection string
+  {
     dialect: 'postgres',
-    protocol: 'postgres',
     dialectOptions: {
-        ssl: {
-            require: true, // Requis pour les connexions sécurisées
-            rejectUnauthorized: false, // Ajustez selon vos besoins de sécurité
-        },
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
     },
     define: {
-        createdAt: 'added',
-        updatedAt: 'updated',
-    },
-});
+      createdAt: 'added',
+      updatedAt: 'updated',
+    }
+  },
+)
 
-// Tester la connexion
 sequelize.authenticate()
-    .then(() => {
-        console.log('Connexion à la base de données réussie !');
-    })
-    .catch((err) => {
-        console.error('Impossible de se connecter à la base de données :', err);
-    });
+sequelize.sync()
 
-// Synchroniser les modèles
-sequelize.sync();
-
-module.exports = sequelize;
+module.exports = sequelize
